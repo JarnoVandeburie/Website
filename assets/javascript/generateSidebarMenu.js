0b724e65fd360edd893ef4ab80ab02ee45f654d1
Jarno-Vandeburie <113904299+Jarno-Vandeburie@users.noreply.github.com>
2023-10-17 10:56:05 +0200

    Endare opdracht 1 basis

--- assets/javascript/generateSidebarMenu.js
+++ assets/javascript/generateSidebarMenu.js
@@ -3,24 +3,20 @@
 document.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-
-    document.querySelector("#sidebarMenu").innerHTML += `
+  document.querySelector("#sidebarMenu").innerHTML += `
     <ul class="sidebarMenuInner">
         <li><a href="index.html">Home</a></li>
+        <li><a href="endare.html">Stage Endare</a></li>
         <li><a href="artikelen.html">Artikelen</a></li>
         <li><a href="opbouw.html">Opbouw site</a></li>
     </ul>
     `;
 
-    await UncheckAll();
+  await UncheckAll();
 }
 
-async function UncheckAll(){
-    setTimeout(function () {
-        document.querySelectorAll('input[type=checkbox]').forEach(box => box.checked = false);
-    }, 2);
-
-
-
-
+async function UncheckAll() {
+  setTimeout(function () {
+    document.querySelectorAll("input[type=checkbox]").forEach(box => (box.checked = false));
+  }, 2);
 }
