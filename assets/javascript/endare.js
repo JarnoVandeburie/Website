dbf284eb6dad7d87d978f3aae06ed4ffcd3497a8
Jarno-Vandeburie <39960443+JarnoVandeburie@users.noreply.github.com>
2023-10-23 10:27:56 +0200

    fix hover over menu text, now no effect

--- assets/javascript/endare.js
+++ assets/javascript/endare.js
@@ -1,8 +1,7 @@
 "use strict";
 
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
 var timer = null;
+var currentId;
 
 document.addEventListener("DOMContentLoaded", init);
 
@@ -27,6 +26,7 @@ function init() {
       const targetElement = document.querySelector(e.target.getAttribute("href"));
 
       targetElement.scrollIntoView();
+      currentId = targetElement.id;
     });
   });
 }
